bfdf0be705f78e9814fb221d64a2ef48d4e57be9
DeadlyRadish <79075921+DeadlyRadish@users.noreply.github.com>
2024-04-16 12:27:09 +0300

    Add files via upload

--- Industry/supplier.cpp
+++ Industry/supplier.cpp
@@ -1,3 +1,41 @@
 #include "supplier.h"
 
-supplier::supplier() {}
+int Supplier::count = 0;
+
+Supplier::Supplier() {
+    name = "Unknow";
+    legal_address = "Unknow";
+    balance = 0.0;
+    date_of_delivery = Date();
+    count++;
+}
+
+Supplier::~Supplier() {
+    count--;
+}
+
+Supplier::Supplier(std::string input_name, std::string input_address, double input_balance, Date input_date) {
+    name = input_name;
+    legal_address = input_address;
+    balance = input_balance;
+    date_of_delivery = input_date;
+    count++;
+}
+
+void Supplier::setName(std::string input_name) {
+    name = input_name;
+}
+
+bool Supplier::setBalance(double input_balance) {
+    if (input_balance > 0) {
+        balance = input_balance;
+        return true;
+    }
+    std::cout << "Невозможно записать отрицательное значение!" << std::endl;
+    return false;
+}
+
+
+std::string Supplier::getName() {
+    return name;
+}
--- Industry/supplier.h
+++ Industry/supplier.h
@@ -1,10 +1,34 @@
 #ifndef SUPPLIER_H
 #define SUPPLIER_H
 
-class supplier
+#include "date.h"
+#include<iostream>
+#include<string>
+
+class Supplier
 {
+private:
+    std::string name;
+    std::string legal_address;
+    double balance;
+    Date date_of_delivery;
 public:
-    supplier();
+    static int count;
+
+    Supplier();
+    Supplier(std::string, std::string, double, Date);
+
+    ~Supplier();
+
+    void setName(std::string);
+    void setAddress(std::string);
+    bool setBalance(double);
+    bool setDate(Date);
+
+    std::string getName();
+    std::string getAddress();
+    double getBalance();
+    Date getDate();
 };
 
 #endif // SUPPLIER_H
